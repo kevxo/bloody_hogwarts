cfab8af56240ae44e4e7d03bdabc3d0bcc28effb
Kevin Cuadros <kev040798@gmail.com>
2020-10-14 22:13:15 -0400

    Finish story 1 and passed all initial tests

---
+++ spec/features/students/index_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'As a visitor' do
+  describe 'when I visit /students' do
+    it 'should list students with their information' do
+      student1 = Student.create!(name: 'John', age: 8, house: 'Slytherian')
+      student2 = Student.create!(name: 'Phil', age: 9, house: 'Ravenclaw')
+      student3 = Student.create!(name: 'Kim', age: 8, house: 'Gryffindor')
+
+      visit '/students'
+
+      expect(page).to have_content(student1.name)
+      expect(page).to have_content(student1.age)
+      expect(page).to have_content(student1.house)
+      expect(page).to have_content(student2.name)
+      expect(page).to have_content(student2.age)
+      expect(page).to have_content(student2.house)
+      expect(page).to have_content(student3.name)
+      expect(page).to have_content(student3.age)
+      expect(page).to have_content(student3.house)
+    end
+  end
+end
